dbe02e8ac1db40ba6ac13a97e0bb784cfbf3cdf1
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-03-30 20:59:09 -0300

    teste handlers

--- Todo.Domain/Handlers/TodoHandler.cs
+++ Todo.Domain/Handlers/TodoHandler.cs
@@ -9,7 +9,10 @@ namespace Todo.Domain.Handlers
 {
     public class TodoHandler :
         Notifiable,
-        IHandler<CreateTodoCommand>
+        IHandler<CreateTodoCommand>,
+        IHandler<UpdateTodoCommand>,
+        IHandler<MarkTodoAsDoneCommand>,
+        IHandler<MarkTodoAsUndoneCommand>
     {
         private readonly ITodoRepository _repository;
 
@@ -27,9 +30,53 @@ namespace Todo.Domain.Handlers
 
             var todo = new TodoItem(command.Title, command.User, command.Date);
             
-            _repository.Create(todo);
+                _repository.Create(todo);
 
             return new GenericCommandResult(true, "Tarefa salva!", todo);
-        }          
+        }
+
+        public ICommandResult Handle(UpdateTodoCommand command)
+        {
+            command.Validate();
+            if(command.Invalid)
+                return new GenericCommandResult(false, "Ops, parece q sua tarefa está errada", command.Notifications);
+
+            var todo = _repository.GetById(command.Id, command.User);
+
+            todo.UpdateTitle(command.Title);
+            _repository.Update(todo);
+
+            return new GenericCommandResult(true, "Tarefa atualizada com sucesso!", todo);
+        }
+
+        public ICommandResult Handle(MarkTodoAsDoneCommand command)
+        {
+            command.Validate();
+            if(command.Invalid)
+                return new GenericCommandResult(false, "Ops, parece q sua tarefa está errada", command.Notifications);
+
+            var todo = _repository.GetById(command.Id, command.User);
+
+            todo.MarkAsDone();
+
+            _repository.Update(todo);
+
+            return new GenericCommandResult(true, "Tarefa atualizada com sucesso!", todo);
+        }
+
+        public ICommandResult Handle(MarkTodoAsUndoneCommand command)
+        {
+            command.Validate();
+            if(command.Invalid)
+                return new GenericCommandResult(false, "Ops, parece q sua tarefa está errada", command.Notifications);
+
+            var todo = _repository.GetById(command.Id, command.User);
+
+            todo.MasAsUndone();
+
+            _repository.Update(todo);
+
+            return new GenericCommandResult(true, "Tarefa atualizada com sucesso!", todo);
+        }
     }
 }
